8ecbe3e72d9871d47a4488a634ed2965f395d34d
Evgenii Kazakov <evgenstf@gmail.com>
2020-09-07 13:55:35 +0300

    [frontend] clean from cache

---
+++ article_manager/run_article_manager_server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+
+PYTHONPATH="$PYTHONPATH:../" python3 article_manager.py
--- frontend/urlshortener/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from urlshortener.models._models import Shortener, SHORTENED_CODE_LENGTH, URL_MAX_LENGTH
-
-
-_all__ = [
-    Shortener,
-    SHORTENED_CODE_LENGTH,
-    URL_MAX_LENGTH,
-]
