b5f601b9c70126ad6dfee31eea201c095fb13b7a
Evgenii Kazakov <evgenstf@gmail.com>
2020-09-14 19:02:28 +0300

    [frontend] remove database settings

--- frontend/settings.py
+++ frontend/settings.py
@@ -76,16 +76,7 @@ WSGI_APPLICATION = 'wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
-}
+DATABASES = { }
 
 
 # Password validation
