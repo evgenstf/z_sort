e19d3f48e750785ddf1c4cc1364281759ee3f686
Evgenii Kazakov <evgenstf@gmail.com>
2020-09-18 12:46:52 +0300

    [frontend] add latex rendering

--- frontend/static/js/article.js
+++ frontend/static/js/article.js
@@ -1,15 +1,1 @@
-/*
-window.onscroll = function() {scrollHandler()};
-
-var header = document.getElementById("z_sort_header");
-var sticky = header.offsetTop;
-
-function scrollHandler() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("fixed_position");
-  } else {
-    header.classList.remove("fixed_position");
-  }
-}
-*/
-
+MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});
