489a5f843e4c39a39c812ea4e4fd3d7863ecb172
Evgenii Kazakov <evgenstf@gmail.com>
2020-09-30 18:22:23 +0300

    [frontend] fix footer binding

--- frontend/static/js/base.js
+++ frontend/static/js/base.js
@@ -1,2 +1,8 @@
+<script>
+var content = document.getElementById("content");
+var ghost_footer = document.getElementById("ghost_footer");
 
+rect = content.getBoundingClientRect();
+ghost_footer.style.height = Math.max(10, window.innerHeight - rect.bottom - 44) + "px";
 
+</script>
