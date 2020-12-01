b4f27737f15302a57c9a5959d08c7816ca1587ea
Evgenii Kazakov <evgenstf@gmail.com>
2020-12-01 00:06:28 +0000

    [html_compiler] make compilation recursive

--- html_compiler/compile.py
+++ html_compiler/compile.py
@@ -6,39 +6,42 @@ import json
 def get_meta_by_path(path):
     return json.loads(open('/'.join(path) + '/meta.json').read())
 
-def compile_article(meta, path):
+def compile_article(path):
     from html_factories.article import ArticleHtmlFactory
-    return ArticleHtmlFactory.create_from_article(get_meta_by_path(path), path, get_meta_by_path(path[:-1]))
 
-def compile_item(meta, path):
+    joined_path = '/'.join(path)
+    with open(joined_path + '/content.html', 'w') as html_file:
+        html_file.write(
+                ArticleHtmlFactory.create_from_article(
+                    get_meta_by_path(path), path, get_meta_by_path(path[:-1])))
+
+    with open(joined_path + '/script.js', 'w') as script_file:
+        script_file.write('{% static js/article.js %}')
+
+    with open(joined_path + '/style.css', 'w') as style_file:
+        style_file.write('{% static css/article.css %}')
+
+def compile_item(path):
+    print(' ', '/'.join(path))
+
+    meta = get_meta_by_path(path)
+
     if meta['type'] == 'article':
-        return compile_article(meta, path)
+        return compile_article(path)
+    elif 'items' in meta:
+        for item in meta['items']:
+            compile_item(path + [item])
 
 def main():
     import argparse
 
     parser = argparse.ArgumentParser()
     parser.add_argument('path', type=str)
-    parser.add_argument('-r', '--recursive', action='store_true')
 
     args = parser.parse_args()
 
-    print('path:', args.path, 'recursive:', args.recursive)
-
-    splitted_path = args.path.split('/')
-    meta = get_meta_by_path(splitted_path)
-    print("meta:", json.dumps(meta, indent=2, sort_keys=True))
-
-    html_result = compile_item(meta, splitted_path)
-
-    with open(args.path + '/content.html', 'w') as html_file:
-        html_file.write(html_result)
-
-    with open(args.path + '/script.js', 'w') as script_file:
-        script_file.write('{% static js/article.js %}')
-
-    with open(args.path + '/style.css', 'w') as style_file:
-        style_file.write('{% static css/article.css %}')
+    print('start compile:')
+    compile_item(args.path.split('/'))
 
 if __name__ == '__main__':
     main()
