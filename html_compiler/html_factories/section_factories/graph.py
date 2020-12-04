863b3167ebf3bb48591fbf08983536ece45ed9b9
leoproko <prokop4uk.lyonya@yandex.ru>
2020-12-04 19:38:26 +0300

    [section_factories] implement graph.py

--- html_compiler/html_factories/section_factories/graph.py
+++ html_compiler/html_factories/section_factories/graph.py
@@ -29,6 +29,10 @@ class DrawGraph:
         self.edges = data['content']['edges']
         self.graph_type = data['content']['type']
         self.show_labels = data['content']['labels']
+        try:
+            self.node_color = data['content']['color']
+        except KeyError:
+            self.node_color = '#BF00B0'
 
     def add_data_to_graph(self, graph):
         for node in range(self.nodes_count):
@@ -41,37 +45,43 @@ class DrawGraph:
         self.add_data_to_graph(graph)
         return graph
 
-    def node_size(self):
+    def calculate_node_diameter(self):
         if 16000 / self.nodes_count > 2000:
             return 2000
         return 16000 / self.nodes_count
 
     def draw(self, path):
         G = self.initialize_graph()
-        plt.figure(figsize=(10,10))
+        plt.figure(figsize=(16,12))
+        plt.axis('off')
         if self.graph_type == 'dot':
             pos = graphviz_layout(G, prog="dot")
         elif self.graph_type == 'twopi':
             pos = graphviz_layout(G, prog="twopi")
         elif self.graph_type == 'circle':
             pos = nx.circular_layout(G)
-        nx.draw_networkx_nodes(G, pos, node_size=self.node_size(), node_color='#BF00B0')
+        nx.draw_networkx_nodes(G, pos, node_size=self.calculate_node_diameter(),\
+                               node_color='#BF00B0')
         nx.draw_networkx_edges(G, pos, edge_color='black', width=4)
         if self.show_labels == 'True':
             nx.draw_networkx_labels(G, pos, font_size=18)
         plt.savefig(path)
+        plt.close()
 
 
 class GraphSectionFactory:
+    graph_id = 0
+
     @staticmethod
     def build_html(section, article_relative_path, static_resources_path):
         article_static_resources_path = static_resources_path + '/articles/' + article_relative_path
         if not os.path.exists(article_static_resources_path):
             os.makedirs(article_static_resources_path)
 
-        graph_absolute_path = article_static_resources_path + '/' + "graph_1.svg"
-        graph_relative_path = article_relative_path + '/' + "graph_1.svg"
+        graph_absolute_path = article_static_resources_path + '/' + "graph_" + str(GraphSectionFactory.graph_id) + "1.svg"
+        graph_relative_path = article_relative_path + '/' + "graph_" + str(GraphSectionFactory.graph_id) + "1.svg"
         graph = DrawGraph(section)
         graph.draw(graph_absolute_path)
+        GraphSectionFactory.graph_id += 1
 
         return "<img src=\"{% static \"articles/" + graph_relative_path + "\" %}\" />"
