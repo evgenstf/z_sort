d9a89ba247ebd50f571df992003701092b301e13
Evgenii Kazakov <evgenstf@gmail.com>
2020-12-06 17:53:33 +0000

    [editor] add tldr section to md_to_sections

--- editor/md_to_sections.py
+++ editor/md_to_sections.py
@@ -13,8 +13,10 @@ def main():
     with open(args.path, 'r') as md_file:
         content = md_file.read()
 
+
         json_content = []
-        json_content.append({"type":"markdown", "content":content})
+        json_content.append({"type":"tldr", "content":content.split("\n\n")[0]})
+        json_content.append({"type":"markdown", "content":"\n\n".join(content.split("\n\n")[1:])})
 
         with open('/'.join(args.path.split('/')[:-1]) + '/sections.json', 'w', encoding='utf-8') as sections_file:
             sections_file.write(json.dumps(json_content, ensure_ascii=False))
