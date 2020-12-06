fdd38215469f3cb5193df6b718bef90b7487d55a
Evgenii Kazakov <evgenstf@gmail.com>
2020-12-06 19:45:53 +0000

    [editor] split markdown to sections

--- editor/md_to_sections.py
+++ editor/md_to_sections.py
@@ -11,15 +11,16 @@ def main():
     print('process', args.path)
 
     with open(args.path, 'r') as md_file:
-        content = md_file.read()
+        content = md_file.read().split("\n\n")
 
 
         json_content = []
-        json_content.append({"type":"tldr", "content":content.split("\n\n")[0]})
-        json_content.append({"type":"markdown", "content":"\n\n".join(content.split("\n\n")[1:])})
+        json_content.append({"type":"tldr", "content":content[0]})
+        for i in range(1, len(content)):
+            json_content.append({"type":"markdown", "content":content[i]})
 
         with open('/'.join(args.path.split('/')[:-1]) + '/sections.json', 'w', encoding='utf-8') as sections_file:
-            sections_file.write(json.dumps(json_content, ensure_ascii=False))
+            sections_file.write(json.dumps(json_content, ensure_ascii=False, indent=2, sort_keys=True))
 
 
 if __name__ == '__main__':
