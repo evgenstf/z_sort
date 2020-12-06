d220fcd598cc83fb89f10820879502c39a657e30
Evgenii Kazakov <evgenstf@gmail.com>
2020-12-06 17:20:09 +0000

    [editor] add md_to_sections.py

---
+++ editor/md_to_sections.py
@@ -0,0 +1,24 @@
+def main():
+    import argparse
+    import os
+    import json
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', type=str)
+
+    args = parser.parse_args()
+
+    print('process', args.path)
+
+    with open(args.path, 'r') as md_file:
+        content = md_file.read()
+
+        json_content = []
+        json_content.append({"type":"markdown", "content":content})
+
+        with open('/'.join(args.path.split('/')[:-1]) + '/sections.json', 'w', encoding='utf-8') as sections_file:
+            sections_file.write(json.dumps(json_content, ensure_ascii=False))
+
+
+if __name__ == '__main__':
+    main()
