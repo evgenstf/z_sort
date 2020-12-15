36b42ec54874330b38d504ac04c15ef6f72726dd
leoproko <prokop4uk.lyonya@yandex.ru>
2020-12-15 14:35:22 +0300

    [seciton_factories] implementig step subsection

--- html_compiler/html_factories/article.py
+++ html_compiler/html_factories/article.py
@@ -4,6 +4,7 @@ import os
 from html_compiler.html_factories.section_factories.markdown import MarkdownSectionFactory
 from html_compiler.html_factories.section_factories.graph import GraphSectionFactory
 from html_compiler.html_factories.section_factories.chart import ChartSectionFactory
+from html_compiler.html_factories.section_factories.steps import StepSectionFactory
 
 def calculate_reading_time(sections):
     total_length = 0
@@ -17,11 +18,7 @@ class ArticleHtmlFactory:
         self.template = None
 
     @staticmethod
-    def build_html(*, meta, absolute_path, relative_path, parent_meta, static_storage_absolute_path):
-        sections = json.loads(open('/'.join(absolute_path) + '/sections.json').read())
-
-        article_header_html = '<br>'.join(meta['header'])
-
+    def compile_section(sections, absolute_path, relative_path, static_storage_absolute_path):
         article_body_html = ''
         for section in sections:
             if section['type'] == 'markdown' or section['type'] == 'tldr':
@@ -36,8 +33,20 @@ class ArticleHtmlFactory:
                         section,
                         '/'.join(relative_path),
                         static_storage_absolute_path)
+            elif section['type'] == 'steps':
+                article_body_html += StepSectionFactory.build_html(section['content'], absolute_path, relative_path, static_storage_absolute_path, ArticleHtmlFactory.compile_section)
             else:
                 print("[warning] unknown section type:", section['type'])
+        return article_body_html
+
+
+    @staticmethod
+    def build_html(*, meta, absolute_path, relative_path, parent_meta, static_storage_absolute_path):
+        sections = json.loads(open('/'.join(absolute_path) + '/sections.json').read())
+
+        article_header_html = '<br>'.join(meta['header'])
+
+        article_body_html = ArticleHtmlFactory.compile_section(sections, absolute_path, relative_path, static_storage_absolute_path)
 
         article_html = open(os.path.dirname(os.path.realpath(__file__)) + '/../templates/html/article.html', 'r').read()
         article_html = article_html.replace('&article_header&', article_header_html)
