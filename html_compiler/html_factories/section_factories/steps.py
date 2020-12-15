94a825fe2decfca0270f67477fa510a0415943e3
leoproko <prokop4uk.lyonya@yandex.ru>
2020-12-15 20:32:40 +0300

    remove step_style.html

--- html_compiler/html_factories/section_factories/steps.py
+++ html_compiler/html_factories/section_factories/steps.py
@@ -1,23 +1,15 @@
 class Step:
     @staticmethod
     def build_first_step(subsection, absolute_path, relative_path, static_storage_absolute_path, compile_section):
-
         step_subbody_html = ''
-
-        with open("/home/leoproko/z_sort/z_sort/html_compiler/html_factories/section_factories/step_static/step_style.html", "r") as step_style:
-            for line in step_style:
-                step_subbody_html += line.strip()
-
         step_subbody_html += '<div class="first-step">'
         step_subbody_html += compile_section([subsection], absolute_path, relative_path, static_storage_absolute_path)
         step_subbody_html += '</div>'
 
         return step_subbody_html
 
-
     @staticmethod
-    def build_middle_step(subsections, absolute_path, relative_path, static_storage_absolute_path, compile_section):
-
+    def build_middle_steps(subsections, absolute_path, relative_path, static_storage_absolute_path, compile_section):
         step_subbody_html = ''
 
         for subsection in subsections[1:-1]:
@@ -27,13 +19,9 @@ class Step:
 
         return step_subbody_html
 
-
-
     @staticmethod
     def build_last_step(subsection, absolute_path, relative_path, static_storage_absolute_path, compile_section):
-
         step_subbody_html = ''
-
         step_subbody_html += '<div class="last-step">'
         step_subbody_html += compile_section([subsection], absolute_path, relative_path, static_storage_absolute_path)
         step_subbody_html += '</div>'
@@ -41,16 +29,13 @@ class Step:
         return step_subbody_html
 
 
-
 class StepSectionFactory:
+     # TODO: remove extra arguments passing
     @staticmethod
     def build_html(subsections, absolute_path, relative_path, static_storage_absolute_path, compile_section):
         step_body_html = ''
-
         step_body_html += Step.build_first_step(subsections[0], absolute_path, relative_path, static_storage_absolute_path, compile_section)
-
-        step_body_html += Step.build_middle_step(subsections, absolute_path, relative_path, static_storage_absolute_path, compile_section)
-
+        step_body_html += Step.build_middle_steps(subsections, absolute_path, relative_path, static_storage_absolute_path, compile_section)
         step_body_html += Step.build_last_step(subsections[-1], absolute_path, relative_path, static_storage_absolute_path, compile_section)
 
         return step_body_html
