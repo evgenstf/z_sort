39bea824a3923bacc4b76a1c0b582ba480f665f6
Evgenii Kazakov <evgenstf@gmail.com>
2020-12-23 01:21:04 +0000

    [frontend_server] hide content till mathjax finish

--- frontend_server/templates/js/base.js
+++ frontend_server/templates/js/base.js
@@ -9,4 +9,11 @@ ghost_footer.style.height = Math.max(10, window.innerHeight - rect.bottom - 44) 
 
 <script type="text/x-mathjax-config">
   MathJax.Hub.Config({"fast-preview": {disabled: true}, tex2jax: {preview: "none", inlineMath: [['$','$'], ['\\(','\\)']]}});
+
+  document.getElementById("content").setAttribute("style", "visibility:hidden");
+  MathJax.Hub.Queue(
+    function () {
+      document.getElementById("content").setAttribute("style", "");
+    }
+  );
 </script>
