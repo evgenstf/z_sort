de607747b95bdc5f577c7d7b06e91b19207206d8
LeoProko <prokop4uk.lyonya@yandex.ru>
2021-01-15 14:51:00 +0300

    [html_compiler] change step design (#12)
    * change step design
    
    * add support of no header in step
    
    * implementation last step
    
    * change steps sequence
    
    * decrease margin-top in steps

--- html_compiler/html_factories/section_factories/steps.py
+++ html_compiler/html_factories/section_factories/steps.py
@@ -3,8 +3,16 @@ def build_first_step(subsections,
     step_subbody_html = ''
 
     step_subbody_html += '<div class="first-step">'
+    step_subbody_html += '<div class="step_number">'
+    step_subbody_html += '1'
+    step_subbody_html += '</div>'
+    if not (subsections[0]['type'] == 'markdown' and subsections[0]['content'][
+        0] == '#'):
+        step_subbody_html += '<h1></h1>'
+    step_subbody_html += '<div class="step_content">'
     step_subbody_html += compile_section(subsections,
             absolute_path, relative_path, static_storage_absolute_path)
+    step_subbody_html += '</div>'
     step_subbody_html += '</div>'
 
     return step_subbody_html
@@ -14,22 +22,43 @@ def build_middle_steps(steps,
         absolute_path, relative_path, static_storage_absolute_path, compile_section):
     step_subbody_html = ''
 
-    for subsections in steps:
-        step_subbody_html += '<div class="middle-step">'
+    for num_step, subsections in enumerate(steps):
+        if num_step % 2 != 0:
+            step_subbody_html += '<div class="middle-step">'
+        else:
+            step_subbody_html += '<div class="middle-step-colorized">'
+        step_subbody_html += '<div class="step_number">'
+        step_subbody_html += str(num_step + 2)
+        step_subbody_html += '</div>'
+        if not (subsections[0]['type'] == 'markdown' and subsections[0]['content'][0] == '#'):
+            step_subbody_html += '<h1></h1>'
+        step_subbody_html += '<div class="step_content">'
         step_subbody_html += compile_section(subsections,
                 absolute_path, relative_path, static_storage_absolute_path)
+        step_subbody_html += '</div>'
         step_subbody_html += '</div>'
 
     return step_subbody_html
 
 
-def build_last_step(subsections,
+def build_last_step(subsections, subsections_length,
         absolute_path, relative_path, static_storage_absolute_path, compile_section):
     step_subbody_html = ''
 
-    step_subbody_html += '<div class="last-step">'
+    if subsections_length % 2 != 0:
+        step_subbody_html += '<div class="last-step">'
+    else:
+        step_subbody_html += '<div class="last-step-colorized">'
+    step_subbody_html += '<div class="step_number">'
+    step_subbody_html += str(subsections_length)
+    step_subbody_html += '</div>'
+    if not (subsections[0]['type'] == 'markdown' and subsections[0]['content'][
+        0] == '#'):
+        step_subbody_html += '<h1></h1>'
+    step_subbody_html += '<div class="step_content">'
     step_subbody_html += compile_section(subsections,
             absolute_path, relative_path, static_storage_absolute_path)
+    step_subbody_html += '</div>'
     step_subbody_html += '</div>'
 
     return step_subbody_html
@@ -46,7 +75,7 @@ class StepSectionFactory:
         if subsections[1:-1]:
             step_body_html += build_middle_steps(subsections[1:-1],
                     absolute_path, relative_path, static_storage_absolute_path, compile_section)
-        step_body_html += build_last_step(subsections[-1],
+        step_body_html += build_last_step(subsections[-1], len(subsections),
                 absolute_path, relative_path, static_storage_absolute_path, compile_section)
 
         return step_body_html
