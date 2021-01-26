68f8dc244d854b8c19bd8d0e2fbc512a21caccf9
Leo Proko <prokop4uk.lyonya@yandex.ru>
2021-01-26 20:48:15 +0300

    add tittle sections

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,9 +1,75 @@
+function add_article_tittle() {
+    let current_section = document.getElementById("editor_window");
+    let article_tittle = document.createElement("div");
+    article_tittle.id = 'article_tittle';
+    let section_name = document.createTextNode("Article tittle");
+
+    article_tittle.appendChild(section_name);
+    current_section.appendChild(article_tittle);
+
+    let form = document.createElement('form')
+    form.method = 'post'
+    form.id = 'article_tittle_form';
+
+    let text_area = document.createElement('textarea');
+    text_area.name = 'text_area';
+    text_area.id = 'article_tittle_text_area';
+
+    form.appendChild(text_area);
+    current_section.appendChild(form);
+}
+add_article_tittle();
+
+function add_article_section() {
+    let current_section = document.getElementById("editor_window");
+    let article_section = document.createElement("div");
+    article_section.id = 'article_section';
+    let section_name = document.createTextNode("Section");
+
+    article_section.appendChild(section_name);
+    current_section.appendChild(article_section);
+
+    let form = document.createElement('form')
+    form.method = 'post'
+    form.id = 'article_section_form';
+
+    let text_area = document.createElement('textarea');
+    text_area.name = 'text_area';
+    text_area.id = 'article_section_text_area';
+
+    form.appendChild(text_area);
+    current_section.appendChild(form);
+}
+add_article_section();
+
+function add_author_name() {
+    let current_section = document.getElementById("editor_window");
+    let author_name = document.createElement("div");
+    author_name.id = 'author_name';
+    let section_name = document.createTextNode("Author");
+
+    author_name.appendChild(section_name);
+    current_section.appendChild(author_name);
+
+    let form = document.createElement('form')
+    form.method = 'post'
+    form.id = 'author_name_form';
+
+    let text_area = document.createElement('textarea');
+    text_area.name = 'text_area';
+    text_area.id = 'author_name_text_area';
+
+    form.appendChild(text_area);
+    current_section.appendChild(form);
+}
+add_author_name();
+
 let num_section = 1;
 function add_section() {
     let current_section = document.getElementById("editor_window");
     let new_section = document.createElement("div");
     new_section.classList.add('section_name');
-    let section_name = document.createTextNode("Markdown ");
+    let section_name = document.createTextNode("Markdown");
 
     new_section.appendChild(section_name);
     current_section.appendChild(new_section);
