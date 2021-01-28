a942edb8eb9a13611586e0b134949638023b814d
Leo Proko <prokop4uk.lyonya@yandex.ru>
2021-01-28 23:20:46 +0300

    update editor header

--- frontend_server/html_factories/base.py
+++ frontend_server/html_factories/base.py
@@ -16,3 +16,22 @@ class BaseHtmlFactory:
         html_template = html_template.replace('&content_css&', content_css)
 
         return html_template
+
+class BaseEditorHtmlFactory:
+    def __init__(self):
+        self.template = None
+
+    @staticmethod
+    def create_from_content(content, content_js, content_css):
+        html_template = open('templates/html/base_editor.html', 'r').read()
+        base_js = open('templates/js/base.js', 'r').read()
+        base_css = open('templates/css/base.css', 'r').read()
+
+        html_template = html_template.replace('&base_js&', base_js)
+        html_template = html_template.replace('&base_css&', base_css)
+
+        html_template = html_template.replace('&content&', content)
+        html_template = html_template.replace('&content_js&', content_js)
+        html_template = html_template.replace('&content_css&', content_css)
+
+        return html_template
