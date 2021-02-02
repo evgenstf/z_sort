e6da48386a0a413a7897ad41bd0af3f3f949a9a1
Leo Proko <prokop4uk.lyonya@yandex.ru>
2021-02-02 15:23:39 +0300

    implement export button

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,17 +1,17 @@
 function add_article_tittle() {
-    current_section = document.getElementById("editor_window");
-    article_tittle = document.createElement("div");
+    let current_section = document.getElementById("editor_window");
+    let article_tittle = document.createElement("div");
     article_tittle.id = 'article_tittle';
-    section_name = document.createTextNode("Article tittle");
+    let section_name = document.createTextNode("Article tittle");
 
     article_tittle.appendChild(section_name);
     current_section.appendChild(article_tittle);
 
-    form = document.createElement('form')
+    let form = document.createElement('form')
     form.method = 'post'
     form.id = 'article_tittle_form';
 
-    text_area = document.createElement('textarea');
+    let text_area = document.createElement('textarea');
     text_area.name = 'text_area';
     text_area.id = 'article_tittle_text_area';
 
@@ -21,18 +21,18 @@ function add_article_tittle() {
 add_article_tittle();
 
 function add_article_section(current_section) {
-    article_section = document.createElement("div");
+    let article_section = document.createElement("div");
     article_section.id = 'article_section';
-    section_name = document.createTextNode("Section");
+    let section_name = document.createTextNode("Section");
 
     article_section.appendChild(section_name);
     current_section.appendChild(article_section);
 
-    form = document.createElement('form')
+    let form = document.createElement('form')
     form.method = 'post'
     form.id = 'article_section_form';
 
-    text_area = document.createElement('textarea');
+    let text_area = document.createElement('textarea');
     text_area.name = 'text_area';
     text_area.id = 'article_section_text_area';
 
@@ -42,18 +42,18 @@ function add_article_section(current_section) {
 }
 
 function add_author_name(current_section) {
-    author_name = document.createElement("div");
+    let author_name = document.createElement("div");
     author_name.id = 'author_name';
-    section_name = document.createTextNode("Author");
+    let section_name = document.createTextNode("Author");
 
     author_name.appendChild(section_name);
     current_section.appendChild(author_name);
 
-    form = document.createElement('form')
+    let form = document.createElement('form')
     form.method = 'post'
     form.id = 'author_name_form';
 
-    text_area = document.createElement('textarea');
+    let text_area = document.createElement('textarea');
     text_area.name = 'text_area';
     text_area.id = 'author_name_text_area';
 
@@ -98,19 +98,7 @@ function add_section() {
     button_up.id = 'move_up_button_' + String(count_sections);
     button_up.num = String(count_sections);
     button_up.onclick = function(){
-        let is_possible_to_move = move_up_section(button_up.num);
-        if (!is_possible_to_move) return;
-        let next_button_up = document.getElementById('move_up_button_' + String(parseInt(button_up.num) - 1));
-        let next_button_down = document.getElementById('move_down_button_' + String(parseInt(button_up.num) - 1));
-        let button_down = document.getElementById('move_down_button_' + button_up.num);
-        next_button_up.num = button_up.num;
-        next_button_up.id = 'move_up_button_' + next_button_up.num;
-        next_button_down.num = button_up.num;
-        next_button_down.id = 'move_down_button_' + next_button_down.num;
-        button_down.num = String(parseInt(button_up.num) - 1);
-        button_down.id = 'move_down_button_' + button_down.num;
-        button_up.num = String(parseInt(button_up.num) - 1);
-        button_up.id = 'move_up_button_' + button_up.num;
+        move_up_section(button_up.num);
     };
     new_section.appendChild(button_up);
     cell.appendChild(new_section);
@@ -125,19 +113,7 @@ function add_section() {
     button_down.id = 'move_down_button_' + String(count_sections);
     button_down.num = String(count_sections);
     button_down.onclick = function(){
-        let is_possible_to_move = move_down_section(button_down.num);
-        if (!is_possible_to_move) return;
-        let next_button_up = document.getElementById('move_up_button_' + String(parseInt(button_down.num) + 1));
-        let next_button_down = document.getElementById('move_down_button_' + String(parseInt(button_down.num) + 1));
-        let button_up = document.getElementById('move_up_button_' + button_down.num);
-        next_button_up.num = button_down.num;
-        next_button_up.id = 'move_up_button_' + next_button_up.num;
-        next_button_down.num = button_down.num;
-        next_button_down.id = 'move_down_button_' + next_button_down.num;
-        button_up.num = String(parseInt(button_down.num) + 1);
-        button_up.id = 'move_up_button_' + button_up.num;
-        button_down.num = String(parseInt(button_down.num) + 1);
-        button_down.id = 'move_down_button_' + button_down.num;
+        move_down_section(button_down.num);
     };
     new_section.appendChild(button_down);
     cell.appendChild(new_section);
@@ -187,7 +163,7 @@ function add_section() {
 }
 add_section();
 
-function compile() {
+function get_sections_from_editor() {
     let sections = []
     for (let index = 1; index < count_sections; ++index) {
         let text = document.getElementById('text_area_' + String(index)).value;
@@ -197,7 +173,11 @@ function compile() {
         }
         sections.push(section)
     }
+    return sections;
+}
 
+function compile() {
+    let sections = get_sections_from_editor();
     $.ajax({
         type: 'post',
         url: '/editor/',
@@ -208,6 +188,16 @@ function compile() {
           article_preview.innerHTML = result['result'];
         }
     });
+}
+
+function export_article() {
+    let sections = get_sections_from_editor();
+    let json_sections = JSON.stringify(sections);
+    let a = document.createElement("a");
+    let file = new Blob([json_sections], {type: 'text/plain'});
+    a.href = URL.createObjectURL(file);
+    a.download = 'article.json';
+    a.click();
 }
 
 function delete_section(num_section) {
@@ -224,8 +214,10 @@ function update_sections_id_after_delete(num_section) {
     for (let i = num_section + 1; i <= editor_sections_list.length; ++i) {
         let button_up = document.getElementById('move_up_button_' + String(num_section));
         let button_down = document.getElementById('move_down_button_' + String(num_section));
+        let text_area = document.getElementById('text_area_' + String(num_section));
         button_up.id = 'move_up_button_' + String(num_section - 1);
         button_down.id = 'move_down_button_' + String(num_section - 1);
+        text_area.id = 'text_area_' + String(num_section - 1);
     }
 }
 
@@ -247,44 +239,82 @@ function update_editor_section(new_editor_section_list) {
     }
 }
 
-function move_up_section(num_section) {
-    let move_up_num_section = parseInt(num_section);
-    if (move_up_num_section === 1) {
+function move_up_section(section_num) {
+    let move_up_section_num = parseInt(section_num);
+    if (move_up_section_num === 1) {
         return false;
     }
     let editor_sections_list = get_editor_sections();
     let new_editor_section_list = [];
-    for (let i = 0; i < 2 * (move_up_num_section - 2); ++i) {
+    for (let i = 0; i < 2 * (move_up_section_num - 2); ++i) {
         new_editor_section_list.push(editor_sections_list[i]);
     }
-    new_editor_section_list.push(editor_sections_list[(move_up_num_section - 1) * 2]);
-    new_editor_section_list.push(editor_sections_list[(move_up_num_section - 1) * 2 + 1]);
-    new_editor_section_list.push(editor_sections_list[(move_up_num_section - 2) * 2]);
-    new_editor_section_list.push(editor_sections_list[(move_up_num_section - 2) * 2 + 1]);
-    for (let i = move_up_num_section * 2; i < editor_sections_list.length; ++i) {
+    new_editor_section_list.push(editor_sections_list[(move_up_section_num - 1) * 2]);
+    new_editor_section_list.push(editor_sections_list[(move_up_section_num - 1) * 2 + 1]);
+    new_editor_section_list.push(editor_sections_list[(move_up_section_num - 2) * 2]);
+    new_editor_section_list.push(editor_sections_list[(move_up_section_num - 2) * 2 + 1]);
+    for (let i = move_up_section_num * 2; i < editor_sections_list.length; ++i) {
         new_editor_section_list.push(editor_sections_list[i]);
     }
     update_editor_section(new_editor_section_list);
-    return true;
+    update_editor_section_id_after_move_up(section_num);
 }
 
-function move_down_section(num_section) {
-    let move_down_num_section = parseInt(num_section);
+function move_down_section(section_num) {
+    let move_down_section_num = parseInt(section_num);
     let editor_sections_list = get_editor_sections();
-    if (move_down_num_section >= editor_sections_list.length / 2) {
-        return false;
+    if (move_down_section_num >= editor_sections_list.length / 2) {
+        return;
     }
     let new_editor_section_list = [];
-    for (let i = 0; i < 2 * (move_down_num_section - 1); ++i) {
+    for (let i = 0; i < 2 * (move_down_section_num - 1); ++i) {
         new_editor_section_list.push(editor_sections_list[i]);
     }
-    new_editor_section_list.push(editor_sections_list[(move_down_num_section) * 2]);
-    new_editor_section_list.push(editor_sections_list[(move_down_num_section) * 2 + 1]);
-    new_editor_section_list.push(editor_sections_list[(move_down_num_section - 1) * 2]);
-    new_editor_section_list.push(editor_sections_list[(move_down_num_section - 1) * 2 + 1]);
-    for (let i = (move_down_num_section + 1) * 2; i < editor_sections_list.length; ++i) {
+    new_editor_section_list.push(editor_sections_list[(move_down_section_num) * 2]);
+    new_editor_section_list.push(editor_sections_list[(move_down_section_num) * 2 + 1]);
+    new_editor_section_list.push(editor_sections_list[(move_down_section_num - 1) * 2]);
+    new_editor_section_list.push(editor_sections_list[(move_down_section_num - 1) * 2 + 1]);
+    for (let i = (move_down_section_num + 1) * 2; i < editor_sections_list.length; ++i) {
         new_editor_section_list.push(editor_sections_list[i]);
     }
     update_editor_section(new_editor_section_list);
-    return true;
+    update_editor_section_id_after_move_down(section_num);
+}
+
+function update_editor_section_id_after_move_down(section_num) {
+    let next_button_up = document.getElementById('move_up_button_' + String(parseInt(section_num) + 1));
+    let next_button_down = document.getElementById('move_down_button_' + String(parseInt(section_num) + 1));
+    let next_text_area = document.getElementById('text_area_' + String(parseInt(section_num) + 1));
+    let button_up = document.getElementById('move_up_button_' + section_num);
+    let button_down = document.getElementById('move_down_button_' + section_num);
+    let text_area = document.getElementById('text_area_' + section_num);
+    next_button_up.num = section_num;
+    next_button_up.id = 'move_up_button_' + next_button_up.num;
+    next_button_down.num = section_num;
+    next_button_down.id = 'move_down_button_' + next_button_down.num;
+    next_text_area.id = 'text_area_' + next_button_down.num;
+    button_up.num = String(parseInt(section_num) + 1);
+    button_up.id = 'move_up_button_' + button_up.num;
+    button_down.num = String(parseInt(section_num) + 1);
+    button_down.id = 'move_down_button_' + button_up.num;
+    text_area.id = 'text_area_' + button_up.num;
+}
+
+function update_editor_section_id_after_move_up(section_num) {
+    let next_button_up = document.getElementById('move_up_button_' + String(parseInt(section_num) - 1));
+    let next_button_down = document.getElementById('move_down_button_' + String(parseInt(section_num) - 1));
+    let next_text_area = document.getElementById('text_area_' + String(parseInt(section_num) - 1));
+    let button_up = document.getElementById('move_up_button_' + section_num);
+    let button_down = document.getElementById('move_down_button_' + section_num);
+    let text_area = document.getElementById('text_area_' + section_num);
+    next_button_up.num = section_num;
+    next_button_up.id = 'move_up_button_' + next_button_up.num;
+    next_button_down.num = section_num;
+    next_button_down.id = 'move_down_button_' + next_button_down.num;
+    next_text_area.id = 'text_area_' + next_button_down.num;
+    button_down.num = String(parseInt(section_num) - 1);
+    button_down.id = 'move_down_button_' + button_down.num;
+    button_up.num = String(parseInt(section_num) - 1);
+    button_up.id = 'move_up_button_' + button_down.num;
+    text_area.id = 'text_area_' + button_down.num;
 }
