b49faad2e4327c7eb76b0ce55b8d471f8019bbf7
LeoProko <prokop4uk.lyonya@yandex.ru>
2021-02-21 19:55:36 +0300

    [storage] implement SQLArticleAdapter (#22)
    Co-authored-by: Evgenii Kazakov <evgenstf@gmail.com>

--- storage/sql_commands.py
+++ storage/sql_commands.py
@@ -3,6 +3,7 @@ import sqlite3
 class SQLCommands:
     @staticmethod
     def get_elements_from_sql_column_by_name(sql_path, table_name, column, name):
+        sql_columns = ['owner', 'email', 'articles_id']
         connection = sqlite3.connect(sql_path)
         cursor = connection.cursor()
         sql_command = 'SELECT * FROM ' + table_name + ' WHERE ' + column + ' = "'  + name + '";'
@@ -13,6 +14,6 @@ class SQLCommands:
         for result in results:
             output_dict = dict()
             for i in range(len(result)):
-                output_dict[self.sql_columns[i]] = result[i]
+                output_dict[sql_columns[i]] = result[i]
             output.append(output_dict)
         return output
