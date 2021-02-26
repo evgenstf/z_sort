1a4ad6b48e6a9dc5727ea8a16069aa9b9d6dbeaf
Leo Proko <prokop4uk.lyonya@yandex.ru>
2021-02-26 14:08:38 +0300

    [frontend_server] add articles preview to user page

--- frontend_server/html_factories/user_page.py
+++ frontend_server/html_factories/user_page.py
@@ -10,7 +10,7 @@ class UserPageHtmlFactory:
         articles_num = len(articles)
         html_articles = ""
         for article in articles:
-            html_articles += article['html']
+            html_articles += article['preview_html']
         user_page_template_html = user_page_template_html.replace('&content&', html_articles)
 
         return articles_num, BaseUserPageHtmlFactory.create_from_content(user_page_template_html)
