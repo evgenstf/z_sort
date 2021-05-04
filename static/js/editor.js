799ac51770feb86c1f20a88cc946f3ac4ae69ae1
Evgenii Kazakov <evgenstf@gmail.com>
2021-05-04 16:54:53 +0100

    wip

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,34 +1,18 @@
-const EDITOR_SECTION_TEMPLATE = `
-<td>
-  <table class="editor-section" width=100% cellspacing="0" cellpadding="0">
-    <tr>
-      <td>
-        <div class="editor-section-type-button">
-          <img src="%section_icon%">
-        </div>
-      </td>
-      <td>
-        <div class="editor-section-move-button">
-          <img src="/sr/svg/icon_move.svg">
-        </div>
-      </td>
-      <td width=10px>
-      </td>
-      <td class="editor-section-content">
-        <div contenteditable="true">
-          %section_content%
-        </div>
-      </td>
-    </tr>
-  </table>
-</td>
-`;
+const EDITOR_SECTION_TEMPLATE = `<table class="editor-section" width=100% cellspacing="0" cellpadding="0" type="%section_type%"><tr><td><div class="editor-section-move-button"><img src="/sr/svg/icon_move.svg"></div></td><td><div class="editor-section-type-button"><img src="%section_icon%"></div></td><td width=10px></td><td class="editor-section-content"><div contenteditable="true">%section_content%</div></td></tr></table>`;
 
 function build_markdown_editor_section(content) {
-  let section_div = document.createElement('tr');
+  let section_div = document.createElement('div');
   let section = EDITOR_SECTION_TEMPLATE;
-  section = EDITOR_SECTION_TEMPLATE.replace('%section_icon%', "/sr/svg/icon_markdown.svg");
-  section = section.replace('%section_content%', content);
+
+  splitted_content = '';
+  for (let line of content.split('\n')) {
+    splitted_content += '<div>' + line + '</div>';
+  }
+
+  section = section.replace('%section_icon%', "/sr/svg/icon_markdown.svg");
+  section = section.replace('%section_content%', splitted_content);
+  section = section.replace('%section_type%', 'markdown');
+
   console.log(section);
   section_div.innerHTML = section;
   return section_div;
@@ -46,20 +30,19 @@ function append_editor_by_section(type, content) {
 }
 
 function get_section_content_from_ui() {
+  let editor_table = document.getElementById("editor-table");
 
+  let section_contents = []
+  for (let node = editor_table.firstChild; node !== null; node = node.nextSibling) {
+    section_contents.push(node.innerText.trim());
+  }
+
+  return section_contents;
 }
 
-append_editor_by_section('markdown', 'asdf');
-append_editor_by_section('markdown', 'asdf');
-append_editor_by_section('markdown', 'asdf');
-append_editor_by_section('markdown', 'asdf');
-
-append_editor_by_section('markdown', 'asdf');
+append_editor_by_section('markdown', 'asdf\nasdf\nkzjxc');
 append_editor_by_section('markdown', 'asdf');
 append_editor_by_section('markdown', 'asdf');
 append_editor_by_section('markdown', 'asdf');
 
-append_editor_by_section('markdown', 'asdf');
-append_editor_by_section('markdown', 'asdf');
-append_editor_by_section('markdown', 'asdf');
-append_editor_by_section('markdown', 'asdf');
+console.log(get_section_content_from_ui());
