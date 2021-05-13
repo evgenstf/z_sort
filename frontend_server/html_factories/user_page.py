b58a71ba8cc0a440c45da9baf865db5451585b2c
york-io <83872715+york-io@users.noreply.github.com>
2021-05-13 21:26:03 +0100

    [user_page] adding dynamic profile picture (#32)
    * user profile pic dynamic
    
    * change1
    
    * change2
    
    * change3
    
    Co-authored-by: york_io <yurii.toma@gmail.com>
    Co-authored-by: Evgenii Kazakov <evgenstf@gmail.com>

--- frontend_server/html_factories/user_page.py
+++ frontend_server/html_factories/user_page.py
@@ -1,4 +1,6 @@
 from html_factories.base import *
+from django.conf import settings
+from django.contrib.staticfiles import finders
 
 from storage.sql_article_connector import SQLArticleConnector
 
@@ -12,5 +14,14 @@ class UserPageHtmlFactory:
         for article in articles:
             html_articles += article['preview_html']
         user_page_template_html = user_page_template_html.replace('&content&', html_articles)
-
+        user_page_template_html = UserPageHtmlFactory.__addProfilePicture(user_page_template_html, username)
+        
         return articles_num, BaseUserPageHtmlFactory.create_from_content(user_page_template_html)
+
+    @staticmethod
+    def __addProfilePicture(html: str, username: str) -> str:
+        expected_profile_picture_relative_path = 'media/profile_pictures/' + username
+        path = settings.STATIC_URL + (expected_profile_picture_relative_path 
+            if finders.find(expected_profile_picture_relative_path) 
+            else 'svg/user_ico.svg')
+        return html.replace('&user_profile_picture&', path)
